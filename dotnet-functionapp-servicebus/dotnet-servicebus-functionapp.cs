e9b6d295430b020d804ffc82c3b9684804eb112f
Gordonby <gordon.byers@microsoft.com>
2022-06-13 15:19:11 +0100

    new version sdk

--- dotnet-functionapp-servicebus/dotnet-servicebus-functionapp.cs
+++ dotnet-functionapp-servicebus/dotnet-servicebus-functionapp.cs
@@ -5,8 +5,11 @@ using Microsoft.Extensions.Logging;
 
 namespace Company.Function
 {
+    
     public static class ServiceBusTrigger
     {
+
+        //The notation %someText% in the trigger attribute indicates that it should be resolved from AppSettings. ServiceBusQueueName should be used in your local.settings.json or in your App Settings Configuration in Azure.
         [FunctionName("ServiceBusTrigger")]
         public static void Run([ServiceBusTrigger("myproj-q",
         Connection = "ServiceBusConnection")]string myQueueItem, ILogger log)
