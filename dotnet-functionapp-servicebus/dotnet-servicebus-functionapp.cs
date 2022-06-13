cc11cddc66738d405954dfd9249d7b00d43d7fbc
Gordonby <gordon.byers@microsoft.com>
2022-06-13 16:46:21 +0100

    renamed function
    Signed-off-by: Gordonby <gordon.byers@microsoft.com>

--- dotnet-functionapp-servicebus/dotnet-servicebus-functionapp.cs
+++ dotnet-functionapp-servicebus/dotnet-servicebus-functionapp.cs
@@ -10,7 +10,7 @@ namespace Company.Function
     {
 
         //The notation %someText% in the trigger attribute indicates that it should be resolved from AppSettings. ServiceBusQueueName should be used in your local.settings.json or in your App Settings Configuration in Azure.
-        [FunctionName("ServiceBusTrigger")]
+        [FunctionName("ReceiveServiceBusMessage")]
         public static void Run([ServiceBusTrigger("myproj-q",
         Connection = "ServiceBusConnection")]string myQueueItem, ILogger log)
         {
